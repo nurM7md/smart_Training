5eaae019a4a89b7c394a7c81391a04d669d0811a
nurM7md <nurmohammed931110@gmail.com>
2023-07-29 03:51:28 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,60 @@
+const products = [
+  {
+    id: 1,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-1.png"),
+  },
+  {
+    id: 2,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-2.png"),
+  },
+  {
+    id: 3,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-3.png"),
+  },
+  {
+    id: 4,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-4.png"),
+  },
+  {
+    id: 5,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-5.png"),
+  },
+  {
+    id: 6,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-6.png"),
+  },
+  {
+    id: 7,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-7.png"),
+  },
+  {
+    id: 8,
+    price: "800 ج.م",
+    desc: "800 ج.م",
+    name: "بلوزة قطنية بيضاء",
+    image: require("./../../../assets/images/product-8.png"),
+  },
+];
+
+
